b33c280101457c8f0e1e345aa0664d8ad703944b
GabrielCalmon <jgabrielcalmon1@hotmail.com>
2020-07-22 18:38:27 -0300

    Áudio para string
    Adição do código de conversão do áudio para string

---
+++ Audio_to_string/Banco_de_Respostas.py
@@ -0,0 +1,13 @@
+lista_erros = [
+    "Não entendi",
+    "Desculpe, mas não entendi",
+    "Você pode repitir?",
+    "Não consegui te ouvir"
+]
+
+lista_inicializacao = [
+    "Estou escutando",
+    "Diga algo",
+    "Olá, seja bem vindo",
+    "O sistema foi inicializado com sucesso"
+]
---
+++ Audio_to_string/Teste.py
@@ -0,0 +1,8 @@
+from funcoes import *
+
+
+intro()
+while True:
+    fala = reconhecimento()
+    print(f'Você disse: {fala}\n')
+    fala_jarvieees(fala)
